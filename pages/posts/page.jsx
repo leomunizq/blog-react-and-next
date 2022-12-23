b3b6a8425c4053d5b92f9e9aa21d126e10d28ebb
leomunizq <leonardo.mdq@hotmail.com>
2022-12-23 18:41:45 +0100

    article page

---
+++ pages/posts/page.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import Format from '../../layout/format'
+import Author from '../../components/_child/author'
+import Image from 'next/image'
+
+export default function page() {
+  return (
+    <Format>
+      <section className="container mx-auto md:px-2 py-16 w-1/2">
+        <div className="flex justify-center">
+          <Author />
+        </div>
+        <div className="post py-10">
+          <h1 className="font-bold text-4xl text-center pb-5">
+            title Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quisquam
+          </h1>
+          <p className="text-gray-500 text-xl text-center">
+            sub-title Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quisquam
+          </p>
+
+          <div className="py-10">
+            <Image src={'/images/img1.jpg'} width={900} height={600}></Image>
+          </div>
+          <div className="content text-gray-600 text-lg flex flex-col gap-4">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            </p>
+          </div>
+        </div>
+      </section>
+    </Format>
+  )
+}
